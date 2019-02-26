8fe4bef14766ebb1f7eba3174f0cde55aec1dd09
Vasiliy Shakhunov <v.shakhunov@gmail.com>
2019-02-26 08:44:54 +0300

    Fix params

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const minterSDK = new Minter();
 const txParams = new SendTxParams({
   privateKey:
     "5fa3a8b186f6cc2d748ee2d8c0eb7a905a7b73de0f2c34c5e7857c3b46f187da",
-  nonce: 1,
+  nonce: 20,
   address: "Mx7633980c000139dd3bd24a3f54e06474fa941e16",
   amount: 10,
   coinSymbol: "MNT",
-  feeCoinSymbol: "ASD",
+  feeCoinSymbol: "MNT",
   gasPrice: 1,
   message: "custom message"
 });
